0bbb97b0bfc461481846fbe605e9d80506be7a05
soum-yaa <140175431+soum-yaa@users.noreply.github.com>
2026-05-15 20:51:44 +0530

    Update script.js

--- script.js
+++ script.js
@@ -343,7 +343,7 @@ async function getWeather(){
     return;
   }
 
-  const apiKey = "YOUR_API_KEY";
+  const apiKey = "YOUR_API_KEY";  // Add your OpenWeather API key here
 
   const url =
     `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
